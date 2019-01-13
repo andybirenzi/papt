39f197f426671cf5fb13c0421415afdf6bc04291
willpapt <wilfriedpapt@gmail.com>
2019-01-13 22:10:03 +0100

    Begin to test

---
+++ src/test/java/birenzi/dev/springbootmongodb/controller/RoleControllerTest.java
@@ -0,0 +1,32 @@
+package birenzi.dev.springbootmongodb.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RoleControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetAllRoles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddOneUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteOneRole() {
+		fail("Not yet implemented");
+	}
+
+}
